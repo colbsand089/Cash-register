e7f698f0e37c2c6a047c858046ee6f9e81f2ae62
Colbey Sands <colbsand089@ed.amdsb.ca>
2020-11-26 13:48:38 -0500

    Add project files.

---
+++ sumative/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace sumative
+{
+    partial class Trash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Trash));
+            this.trashyTimsLabel = new System.Windows.Forms.Label();
+            this.coffeeLabel = new System.Windows.Forms.Label();
+            this.donutLabel = new System.Windows.Forms.Label();
+            this.trashBitLabel = new System.Windows.Forms.Label();
+            this.coffeInputTextbox = new System.Windows.Forms.TextBox();
+            this.donutInputTextbox = new System.Windows.Forms.TextBox();
+            this.trashbitsInputTextbox = new System.Windows.Forms.TextBox();
+            this.printLabel = new System.Windows.Forms.Label();
+            this.calculateTotalsButton = new System.Windows.Forms.Button();
+            this.subTotalLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.subTotalCost = new System.Windows.Forms.Label();
+            this.taxCost = new System.Windows.Forms.Label();
+            this.totalCost = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.calculateChangeButton = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.changeAmount = new System.Windows.Forms.Label();
+            this.printButton = new System.Windows.Forms.Button();
+            this.tenderedTextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // trashyTimsLabel
+            // 
+            this.trashyTimsLabel.Font = new System.Drawing.Font("Pioneer BT", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trashyTimsLabel.Location = new System.Drawing.Point(26, -1);
+            this.trashyTimsLabel.Name = "trashyTimsLabel";
+            this.trashyTimsLabel.Size = new System.Drawing.Size(571, 86);
+            this.trashyTimsLabel.TabIndex = 0;
+            this.trashyTimsLabel.Text = "Trashy Tims";
+            this.trashyTimsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // coffeeLabel
+            // 
+            this.coffeeLabel.AutoEllipsis = true;
+            this.coffeeLabel.Font = new System.Drawing.Font("Millenium BdEx BT", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coffeeLabel.Location = new System.Drawing.Point(38, 133);
+            this.coffeeLabel.Name = "coffeeLabel";
+            this.coffeeLabel.Size = new System.Drawing.Size(74, 23);
+            this.coffeeLabel.TabIndex = 1;
+            this.coffeeLabel.Text = "Coffee";
+            // 
+            // donutLabel
+            // 
+            this.donutLabel.AutoEllipsis = true;
+            this.donutLabel.Font = new System.Drawing.Font("Millenium BdEx BT", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.donutLabel.Location = new System.Drawing.Point(38, 165);
+            this.donutLabel.Name = "donutLabel";
+            this.donutLabel.Size = new System.Drawing.Size(74, 23);
+            this.donutLabel.TabIndex = 2;
+            this.donutLabel.Text = "Donut";
+            // 
+            // trashBitLabel
+            // 
+            this.trashBitLabel.AutoEllipsis = true;
+            this.trashBitLabel.Font = new System.Drawing.Font("Millenium BdEx BT", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trashBitLabel.Location = new System.Drawing.Point(-3, 193);
+            this.trashBitLabel.Name = "trashBitLabel";
+            this.trashBitLabel.Size = new System.Drawing.Size(115, 23);
+            this.trashBitLabel.TabIndex = 3;
+            this.trashBitLabel.Text = "Trash Bits";
+            // 
+            // coffeInputTextbox
+            // 
+            this.coffeInputTextbox.Font = new System.Drawing.Font("mono 07_55", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coffeInputTextbox.Location = new System.Drawing.Point(118, 131);
+            this.coffeInputTextbox.Name = "coffeInputTextbox";
+            this.coffeInputTextbox.Size = new System.Drawing.Size(35, 25);
+            this.coffeInputTextbox.TabIndex = 4;
+            // 
+            // donutInputTextbox
+            // 
+            this.donutInputTextbox.Location = new System.Drawing.Point(118, 163);
+            this.donutInputTextbox.Name = "donutInputTextbox";
+            this.donutInputTextbox.Size = new System.Drawing.Size(35, 20);
+            this.donutInputTextbox.TabIndex = 5;
+            // 
+            // trashbitsInputTextbox
+            // 
+            this.trashbitsInputTextbox.Location = new System.Drawing.Point(118, 191);
+            this.trashbitsInputTextbox.Name = "trashbitsInputTextbox";
+            this.trashbitsInputTextbox.Size = new System.Drawing.Size(35, 20);
+            this.trashbitsInputTextbox.TabIndex = 6;
+            // 
+            // printLabel
+            // 
+            this.printLabel.BackColor = System.Drawing.Color.White;
+            this.printLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.printLabel.Font = new System.Drawing.Font("mono 07_56", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.printLabel.Location = new System.Drawing.Point(275, 114);
+            this.printLabel.Name = "printLabel";
+            this.printLabel.Size = new System.Drawing.Size(332, 327);
+            this.printLabel.TabIndex = 7;
+            // 
+            // calculateTotalsButton
+            // 
+            this.calculateTotalsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateTotalsButton.Location = new System.Drawing.Point(41, 233);
+            this.calculateTotalsButton.Name = "calculateTotalsButton";
+            this.calculateTotalsButton.Size = new System.Drawing.Size(185, 23);
+            this.calculateTotalsButton.TabIndex = 8;
+            this.calculateTotalsButton.Text = "Calculate Totals";
+            this.calculateTotalsButton.UseVisualStyleBackColor = true;
+            this.calculateTotalsButton.Click += new System.EventHandler(this.CalculateTotalsButton_Click);
+            // 
+            // subTotalLabel
+            // 
+            this.subTotalLabel.AutoSize = true;
+            this.subTotalLabel.Location = new System.Drawing.Point(23, 273);
+            this.subTotalLabel.Name = "subTotalLabel";
+            this.subTotalLabel.Size = new System.Drawing.Size(53, 13);
+            this.subTotalLabel.TabIndex = 9;
+            this.subTotalLabel.Text = "Sub Total";
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Location = new System.Drawing.Point(23, 303);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(25, 13);
+            this.taxLabel.TabIndex = 10;
+            this.taxLabel.Text = "Tax";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(23, 331);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(31, 13);
+            this.totalLabel.TabIndex = 11;
+            this.totalLabel.Text = "Total";
+            // 
+            // subTotalCost
+            // 
+            this.subTotalCost.AutoSize = true;
+            this.subTotalCost.Location = new System.Drawing.Point(189, 273);
+            this.subTotalCost.Name = "subTotalCost";
+            this.subTotalCost.Size = new System.Drawing.Size(0, 13);
+            this.subTotalCost.TabIndex = 12;
+            // 
+            // taxCost
+            // 
+            this.taxCost.AutoSize = true;
+            this.taxCost.Location = new System.Drawing.Point(189, 303);
+            this.taxCost.Name = "taxCost";
+            this.taxCost.Size = new System.Drawing.Size(0, 13);
+            this.taxCost.TabIndex = 13;
+            // 
+            // totalCost
+            // 
+            this.totalCost.AutoSize = true;
+            this.totalCost.Location = new System.Drawing.Point(189, 331);
+            this.totalCost.Name = "totalCost";
+            this.totalCost.Size = new System.Drawing.Size(0, 13);
+            this.totalCost.TabIndex = 14;
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.AutoSize = true;
+            this.tenderedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedLabel.Location = new System.Drawing.Point(12, 371);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(68, 15);
+            this.tenderedLabel.TabIndex = 15;
+            this.tenderedLabel.Text = "Tendered";
+            // 
+            // calculateChangeButton
+            // 
+            this.calculateChangeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateChangeButton.Location = new System.Drawing.Point(79, 397);
+            this.calculateChangeButton.Name = "calculateChangeButton";
+            this.calculateChangeButton.Size = new System.Drawing.Size(123, 23);
+            this.calculateChangeButton.TabIndex = 17;
+            this.calculateChangeButton.Text = "Calculate Change";
+            this.calculateChangeButton.UseVisualStyleBackColor = true;
+            this.calculateChangeButton.Click += new System.EventHandler(this.CalculateChangeButton_Click);
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.Location = new System.Drawing.Point(58, 428);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(44, 13);
+            this.changeLabel.TabIndex = 18;
+            this.changeLabel.Text = "Change";
+            // 
+            // changeAmount
+            // 
+            this.changeAmount.AutoSize = true;
+            this.changeAmount.Location = new System.Drawing.Point(153, 428);
+            this.changeAmount.Name = "changeAmount";
+            this.changeAmount.Size = new System.Drawing.Size(0, 13);
+            this.changeAmount.TabIndex = 19;
+            // 
+            // printButton
+            // 
+            this.printButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.printButton.Location = new System.Drawing.Point(378, 88);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(116, 23);
+            this.printButton.TabIndex = 20;
+            this.printButton.Text = "PRINT";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.PrintButton_Click);
+            // 
+            // tenderedTextbox
+            // 
+            this.tenderedTextbox.Location = new System.Drawing.Point(89, 370);
+            this.tenderedTextbox.Name = "tenderedTextbox";
+            this.tenderedTextbox.Size = new System.Drawing.Size(100, 20);
+            this.tenderedTextbox.TabIndex = 21;
+            // 
+            // Trash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 450);
+            this.Controls.Add(this.tenderedTextbox);
+            this.Controls.Add(this.printButton);
+            this.Controls.Add(this.changeAmount);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.calculateChangeButton);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.totalCost);
+            this.Controls.Add(this.taxCost);
+            this.Controls.Add(this.subTotalCost);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subTotalLabel);
+            this.Controls.Add(this.calculateTotalsButton);
+            this.Controls.Add(this.printLabel);
+            this.Controls.Add(this.trashbitsInputTextbox);
+            this.Controls.Add(this.donutInputTextbox);
+            this.Controls.Add(this.coffeInputTextbox);
+            this.Controls.Add(this.trashBitLabel);
+            this.Controls.Add(this.donutLabel);
+            this.Controls.Add(this.coffeeLabel);
+            this.Controls.Add(this.trashyTimsLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Trash";
+            this.Text = "Cash Register";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label trashyTimsLabel;
+        private System.Windows.Forms.Label coffeeLabel;
+        private System.Windows.Forms.Label donutLabel;
+        private System.Windows.Forms.Label trashBitLabel;
+        private System.Windows.Forms.TextBox coffeInputTextbox;
+        private System.Windows.Forms.TextBox donutInputTextbox;
+        private System.Windows.Forms.TextBox trashbitsInputTextbox;
+        private System.Windows.Forms.Label printLabel;
+        private System.Windows.Forms.Button calculateTotalsButton;
+        private System.Windows.Forms.Label subTotalLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label subTotalCost;
+        private System.Windows.Forms.Label taxCost;
+        private System.Windows.Forms.Label totalCost;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.Button calculateChangeButton;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Label changeAmount;
+        private System.Windows.Forms.Button printButton;
+        private System.Windows.Forms.TextBox tenderedTextbox;
+    }
+}
+
---
+++ sumative/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+namespace sumative
+{
+    public partial class Trash : Form
+    {
+        // November 26, 2020
+        double taxRate = 0.13;
+        double coffeeCost = 1.45;
+        double donutCost = 3.65;
+        double trashbitCost =9.00;
+        double numberOfCoffee = 0;
+        double numberOfDonuts = 0;
+        double numberOfTrashbits = 0;
+        double costOfOrder = 0;
+        double taxAmount = 0;
+        double costOfOrderWithTax = 0;
+        double tendered;
+        double changeBack = 0;
+        public Trash()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateTotalsButton_Click(object sender, EventArgs e)
+        {
+            // Calculations for user imput boxes.
+
+            numberOfCoffee = Convert.ToInt32(coffeInputTextbox.Text);
+            numberOfDonuts = Convert.ToInt32(donutInputTextbox.Text);
+            numberOfTrashbits = Convert.ToInt32(trashbitsInputTextbox.Text);
+
+            // Calculations for $ amounts.
+            costOfOrder = coffeeCost * numberOfCoffee + donutCost * numberOfDonuts + trashbitCost * numberOfTrashbits;
+            taxAmount = costOfOrder * taxRate;
+            costOfOrderWithTax = costOfOrder + taxAmount;
+
+            // Calculations for final $ amounts.
+            subTotalCost.Text = $"{costOfOrder.ToString("$.00")}";
+            taxCost.Text = $"{taxAmount.ToString("$.00")}";
+            totalCost.Text = $"{costOfOrderWithTax.ToString("$.00")}";
+        }
+
+        private void PrintButton_Click(object sender, EventArgs e)
+        {
+            // Things shown on the printer.
+            SoundPlayer player = new SoundPlayer(Properties.Resources.Dot_Matrix_Printer_SoundBible_com_790333844);
+            player.Play();
+            printLabel.Text = $"Coffee:                   {numberOfCoffee}";
+            Refresh();
+            Thread.Sleep(400);
+
+            player.Play();
+            printLabel.Text += $"\nDonuts:                   {numberOfDonuts}";
+            Refresh();
+            Thread.Sleep(400);
+
+            player.Play();
+            printLabel.Text += $"\nTrashbits:                {numberOfTrashbits}";
+            Refresh();
+            Thread.Sleep(400);
+
+            player.Play(); 
+            printLabel.Text += $"\n\nSub Total:                {costOfOrder.ToString("$.00")}";
+            Refresh();
+            Thread.Sleep(400);
+
+            player.Play();
+            printLabel.Text += $"\nTax:                      {taxAmount.ToString("$.00")}";
+            Refresh();
+            Thread.Sleep(400);
+
+            player.Play();
+            printLabel.Text += $"\nTotal Cost:               {costOfOrderWithTax.ToString("$.00")}";
+            Refresh();
+            Thread.Sleep(400);
+
+            player.Play();
+            printLabel.Text += $"\n\nTendered                  {tendered.ToString("$.00")}";
+            Refresh();
+            Thread.Sleep(400);
+
+            player.Play();
+            printLabel.Text += $"\nChange back               {changeBack.ToString("$.00")}";
+            Refresh();
+            Thread.Sleep(400);
+
+            printLabel.Text += $"\n\n        Have a trash day!";
+        }
+
+        private void CalculateChangeButton_Click(object sender, EventArgs e)
+        {
+            // Calulations for tendered and change.
+            tendered = Convert.ToDouble(tenderedTextbox.Text);
+            changeBack = tendered - costOfOrderWithTax;
+            changeAmount.Text = $"{changeBack.ToString("$.00")}";
+        }
+    }
+}
